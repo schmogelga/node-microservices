87be4260ec8e5ac790105fc1db5719500b2d4f93
Gabriel Augusto Schmogel <gabriel.schmogel@dbserver.com.br>
2023-05-10 06:58:11 -0300

    feat: implementado CRUD de Account

--- src/services/account/module/account.controller.ts
+++ src/services/account/module/account.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Inject, Post } from "@nestjs/common";
+import { Body, Controller, Inject, Post, Get, Delete, Put, Param } from "@nestjs/common";
+import { Account } from "../../../entity/account.entity";
 import { AccountService } from "./account.service";
 
-@Controller('accounts')
+@Controller( 'accounts' )
 export class AccountController {
 
     constructor( @Inject( AccountService ) private accountService: AccountService ){}
@@ -10,4 +11,26 @@ export class AccountController {
     createAccount( @Body() body: { username: string, email: string } ){
         this.accountService.createAccount( body.username, body.email );
     }
+
+    @Get()
+    getAllAccounts(){
+        this.accountService.getAllAccounts();
+    }
+
+    @Get( ':id' )
+    getAccount( @Param( 'id' ) accountId: number ){
+        this.accountService.geAccount( accountId );
+    }
+
+    @Delete( ':id' )
+    async deleteAccount( @Param( 'id' ) accountId: number ): Promise<void>{
+        
+        return this.accountService.deleteAccount( accountId );
+    }
+
+    @Put( ':id' )
+    async updateAccount( @Param( 'id' ) accountId: number, @Body() account: Account ): Promise<Account>{
+        return this.accountService.updateAccount( accountId, account );
+    }
+
 }
--- src/services/account/module/account.service.ts
+++ src/services/account/module/account.service.ts
@@ -1,22 +1,47 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
+import { ObjectId, Repository } from "typeorm";
 import { Account } from "../../../entity/account.entity";
 import { AccountRepository } from "./account.repository";
 
 @Injectable()
 export class AccountService {
-
+    
     constructor( @InjectRepository(Account) private accountRepositorry: AccountRepository ){}
+    
+    async updateAccount( id: number, account: Account ): Promise<Account> {
+        await this.accountRepositorry.update( id, account );
+        return await this.geAccount( id );
+    }
+    
+    async deleteAccount( id: number ): Promise<void> {
+        
+        await this.geAccount( id );
+        await this.accountRepositorry.delete( id );
+    }
 
-    createAccount( username: string, email: string ){
+    async geAccount( id: number ): Promise<Account> {
+    
+        const account = await this.accountRepositorry.findOne( { where: { id } } );
+        if( !account ){
+            throw new Error( "User not found" );
+        }
+        
+        return account;
+    }
+    async getAllAccounts(): Promise<Account[]> {
+        return await this.accountRepositorry.find();
+    }
 
-        const account = new Account();
+    async createAccount( username: string, email: string ): Promise<Account>{
+
+        let account = new Account();
 
         account.email = email;
         account.username = username;
 
-        this.accountRepositorry.save( account );
+        account = this.accountRepositorry.create( account )
+        return await this.accountRepositorry.save( account );
     }
 }
 
